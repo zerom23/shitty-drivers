7e8cdd7d16f05cf7cdae1b136d7b3edb0c117497
zerom23 <103960337+zerom23@users.noreply.github.com>
2023-02-28 17:15:36 -0800

    Update header.h
    added some more code

--- header.h
+++ header.h
@@ -1,10 +1,12 @@
-NTSTATUS ObjectEntry(PDEVICE_OBJECT DriverObject, PUNICODE_STRING Driver)
+NTSTATUS DriverLoad(PDEVICE_OBJECT DriverObject, PUNICODE_STRING Driver)
 {
 	return STATUS_SUCCESS;
+	ObjectEntry->DriverObject = DriverLoad;
 }
 
 
 NTSTATUS DriverUnload(PDEVICE_OBJECT UnloadDriver,PUNICODE_STRING Drivers)
 {
+	UnloadDriver->Drivers = DriverUnload;
 	return STATUS_SUCCESS;
 }
